fb1f1166c8868c08511fbbb257281371cfcfe278
Ertugrul Mac <ertugrulmac@192.168.1.167>
2026-07-04 21:39:13 +0300

    chore: setup eas and sync package-lock

---
+++ GeoCart/GeoCart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeoCart
+//
+//  Created by Ertugrul Mac on 28.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeoCart/GeoCart/GeoCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoCartApp.swift
+//  GeoCart
+//
+//  Created by Ertugrul Mac on 28.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct GeoCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
